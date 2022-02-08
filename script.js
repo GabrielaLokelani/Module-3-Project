aff9cf3668e0790550bb7f835128ff6bb6f0fadc
uc-Manny <manny.muro@gmail.com>
2022-02-08 00:12:41 -0800

    Initial check-in of the 'script.js' file into the project's repo

---
+++ script.js
@@ -0,0 +1,29 @@
+// This is where the various JS function calls and event handlers will be located
+
+function main() {
+    // Place the initial start-up code here
+}
+
+function about(){
+    // Place JS code here to handle the About screen/view
+}
+
+function game_options() {
+    // Place code here to handle the game options
+}
+
+function game_play() {
+    // place code here to handle the actual game play screen and functionality
+}
+
+function game_start() {
+    // Place code here to handle when the game starts up, i.e. at time equal to zero
+}
+
+function game_stop(){
+    // Place code here to handle when the end of the game is detected.
+}
+
+function game_pause(){
+    // Place code here to handle how the game handles needing to be paused.
+}
